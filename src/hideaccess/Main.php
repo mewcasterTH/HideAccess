70ed0504e37f2a150e02a85fc842810d7633d4f3
HeavenPE-Developers <50451928+HeavenPE@users.noreply.github.com>
2019-05-14 17:09:05 +0400

    (^-^)

---
+++ src/hideaccess/Main.php
@@ -0,0 +1,32 @@
+<?php
+namespace hideaccess;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\server\DataPacketSendEvent;
+use pocketmine\network\mcpe\protocol\AvailableCommandsPacket;
+class HideExcess extends PluginBase implements Listener
+{
+    public function onEnable()
+    {
+        $this->getServer()->getLogger()->info("HideExcess By HeavenPE started");
+        $this->getServer()->getLogger()->info("Discord.HeavenPE.Cf");
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+    public function onSendPacket(DataPacketSendEvent $event){
+        $packet = $event->getPacket();
+        $player = $event->getPlayer();
+        if($packet instanceof AvailableCommandsPacket){
+            $data = [];
+            foreach($this->plugin->getServer()->getCommandMap()->getCommands() as $command){
+                if($player->hasPermission($command->getPermission())){
+                    if(count($cmdData = $command->generateCustomCommandData($player)) > 0){
+                        $data[$command->getName()]["versions"][0] = $cmdData;
+                    }
+                }
+            }
+            if(count($data) > 0){
+                $packet->commands = json_encode($data);
+            }
+        }
+    }
+}
